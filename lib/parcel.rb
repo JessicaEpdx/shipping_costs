3d50076d2ce554104cc49f8dd2bb8ae6a7834c3c
Jessica Engel & Franz Knupfer <student@epicodus.com>
2015-04-27 15:35:47 -0700

    return cost of shipping based soley on weight

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,12 +1,22 @@
 class Parcel
   define_method(:initialize) do |weight, length, width, height, rarity|
-    @weight=weight
+    @weight = weight
+    @length = length
+    @width = width
+    @height = height
+    @rarity = rarity
   end
 
-  define_method(:shipping_cost) do
-    @weight
+  define_method(:weight_shipping_cost) do
+    weight_cost = 0
+    if @weight.<=(16)
+      weight_cost = 100
+    elsif @weight.>(16) && @weight.<=(32)
+      weight_cost = 120
+    else
+      weight_cost = 140
+    end
+
   end
-
-
 
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -5,7 +5,7 @@ describe(Parcel) do
   describe("#shipping_cost") do
     it("will return a cost based on the weight of a package of rare orchids") do
       test_parcel=Parcel.new(16, 1, 1, 1, "uncommon")
-      expect(test_parcel.shipping_cost()).to(eq(100))
+      expect(test_parcel.weight_shipping_cost()).to(eq(100))
     end
   end
 end
