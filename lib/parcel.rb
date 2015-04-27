8952a7117109452e8647535ef35deb75df8e3921
Jessica Engel & Franz Knupfer <student@epicodus.com>
2015-04-27 15:45:35 -0700

    returns shipping cost based on weight AND box size

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,8 +1,6 @@
 class Parcel
-  define_method(:initialize) do |weight, length, width, height, rarity|
+  define_method(:initialize) do |weight, height, rarity|
     @weight = weight
-    @length = length
-    @width = width
     @height = height
     @rarity = rarity
   end
@@ -16,7 +14,24 @@ class Parcel
     else
       weight_cost = 140
     end
-
+    @weight_cost = weight_cost
   end
+
+  define_method(:box_shipping_cost) do
+    box_cost = 0
+    if @height.<=(12)
+      box_cost = 25
+    elsif @height.<=(18)
+      box_cost = 30
+    elsif @height.<=(24)
+      box_cost = 35
+    else
+      box_cost = 40
+    end
+    @box_cost=box_cost
+  end
+
+    define_method(:shipping_cost) do
+    end
 
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,8 +4,12 @@ require('parcel.rb')
 describe(Parcel) do
   describe("#shipping_cost") do
     it("will return a cost based on the weight of a package of rare orchids") do
-      test_parcel=Parcel.new(16, 1, 1, 1, "uncommon")
+      test_parcel=Parcel.new(16, 1, "uncommon")
       expect(test_parcel.weight_shipping_cost()).to(eq(100))
+    end
+    it("will return a cost based on the box size of a package of rare orchids") do
+      test_parcel=Parcel.new(16, 12, "uncommon")
+      expect(test_parcel.box_shipping_cost()).to(eq(25))
     end
   end
 end
